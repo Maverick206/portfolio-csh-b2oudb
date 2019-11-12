a3491d0b7a76b3502fd7b1c4412a45ee4aff3f7b
Maverick206 <garciamaverick25@gmail.com>
2019-11-12 13:24:13 -0800

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,13 +11,15 @@ import { CshbotComponent } from './cshbot/cshbot.component';
 
 import { CalcService } from '../calc.service';
 import { CshbotService } from '../cshbot.service';
+import { Mg-something-cute } from '../mg-something-cute/mg-something-cute/component';
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, RouterModule.forRoot([
     { path: '', component: HomeComponent },
     { path: 'about', component: AboutComponent },
-    { path: 'cshbot', component: CshbotComponent }
+    { path: 'cshbot', component: CshbotComponent },
+    { path: 'mg-something-cute', component: mg-something-cute}
   ]) ],
   declarations: [ AppComponent, HelloComponent, HomeComponent, AboutComponent, CshbotComponent ],
   providers: [CalcService, CshbotService],
---
+++ src/mg-something-cute/mg-something-cute.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mg-something-cute',
+  templateUrl: './mg-something-cute.component.html',
+  styleUrls: ['./mg-something-cute.component.css']
+})
+export class MGSomethingCuteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
